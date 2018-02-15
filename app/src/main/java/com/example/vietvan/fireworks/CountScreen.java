421592aee2d14a625359b84b7fc8d8b7b5bf2ee6
mrvodah <vietvandev@gmail.com>
2018-02-15 19:53:44 +0700

    update

--- app/src/main/java/com/example/vietvan/fireworks/CountScreen.java
+++ app/src/main/java/com/example/vietvan/fireworks/CountScreen.java
@@ -5,25 +5,41 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.os.Handler;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.TextView;
+
+import com.plattysoft.leonids.ParticleSystem;
+
+import java.util.Random;
 
 public class CountScreen extends Activity {
 
-    TextView text;
+    TextView text, happynewyear, tx3, tx4, tx5, tx6, tx7;
     int number = 7;
     MediaPlayer mediaPlayer;
-
+    int timer = 0;
+    boolean x = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_count_screen);
 
+        happynewyear = findViewById(R.id.happynewyear);
         text = findViewById(R.id.textview);
+        tx3 = findViewById(R.id.textView3);
+        tx4 = findViewById(R.id.textView4);
+        tx5 = findViewById(R.id.textView5);
+        tx6 = findViewById(R.id.textView6);
+        tx7 = findViewById(R.id.textView7);
         Typeface type = Typeface.createFromAsset(getAssets(), "fonts/From_Cartoon_Blocks.ttf");
         text.setTypeface(type);
+        Typeface type2 = Typeface.createFromAsset(getAssets(), "fonts/UTM_Dai_Co_Viet.ttf");
+        happynewyear.setTypeface(type2);
         mediaPlayer = MediaPlayer.create(this, R.raw.countdowntimer);
         mediaPlayer.start();
 
--- app/src/main/java/com/example/vietvan/fireworks/MainActivity.java
+++ app/src/main/java/com/example/vietvan/fireworks/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
+import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,20 +15,38 @@ import android.view.animation.AccelerateInterpolator;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.plattysoft.leonids.ParticleSystem;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+import pl.droidsonroids.gif.GifTextView;
+
 public class MainActivity extends Activity{
 
+    GifTextView gif;
     RelativeLayout relativeLayout;
     MediaPlayer mediaPlayer, mediaSound;
+    int song[] = new int[]{
+            R.raw.auld_lang_syne, R.raw.auld_lang_syne, R.raw.mylove
+    };
+    int currentsong = 0;
     SoundPool soundPool;
-    int time = 1, delay = 1001;
+    int time = 1, delay = 1001, pos = 0;
+    int[][] view = new int[][]{
+            {R.drawable.s_red, R.drawable.s_green, R.drawable.s_blue, R.drawable.s_white, R.drawable.s_yellow, R.drawable.s_pink, R.drawable.s_purple},
+            {R.drawable.autumn_red, R.drawable.autume_green, R.drawable.autume_blue, R.drawable.autume_white, R.drawable.autumn_yellow, R.drawable.autume_pink, R.drawable.autume_purple},
+            {R.drawable.heart_red, R.drawable.heart_green, R.drawable.heart_blue, R.drawable.heart_white, R.drawable.heart_yellow, R.drawable.heart_pink, R.drawable.heart_purple},
+            {R.drawable.snow_red, R.drawable.snow_green, R.drawable.snow_blue, R.drawable.snow_white, R.drawable.snow_yellow, R.drawable.snow_pink, R.drawable.snow_purple},
+            {R.drawable.light_red, R.drawable.light_green, R.drawable.light_blue, R.drawable.light_white, R.drawable.light_yellow, R.drawable.light_pink, R.drawable.light_purple},
+            {R.drawable.moon_red, R.drawable.moon_green, R.drawable.moon_blue, R.drawable.moon_white, R.drawable.moon_yellow, R.drawable.moon_pink, R.drawable.moon_purple},
+            {R.drawable.bat_red, R.drawable.bat_green, R.drawable.bat_blue, R.drawable.bat_white, R.drawable.bat_yellow, R.drawable.bat_pink, R.drawable.bat_purple},
+    };
     Handler handler;
     Distich[] array = new Distich[]{
             new Distich("Tân niên tân phúc tân tri ký", "Vạn lộc vạn tài vạn công danh"),
@@ -45,8 +64,9 @@ public class MainActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gif = findViewById(R.id.gif);
         relativeLayout = (RelativeLayout)findViewById(R.id.relative);
-        mediaPlayer = MediaPlayer.create(this,R.raw.happy_new_year_abba);
+        mediaPlayer = MediaPlayer.create(this,song[currentsong]);
         mediaSound = MediaPlayer.create(this,R.raw.sound);
         mediaPlayer.start();
         mediaSound.start();
@@ -86,8 +106,23 @@ public class MainActivity extends Activity{
         };
         thread.start();
 
+        gif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random r= new Random();
+                pos = r.nextInt(7);
+            }
+        });
+
         randomUI(width, height);
         //randomSnow();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mediaSound.stop();
+        mediaPlayer.stop();
     }
 
     private void randomUI(final int width, final int height) {
@@ -106,8 +141,14 @@ public class MainActivity extends Activity{
                 tx.setTop((int)y * 2);
                 tx.setLeft((int)x * 2);
                 relativeLayout.addView(tx, params);
-                if(time > 45){
-                    delay = 1300;
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        mediaPlayer.start();
+                    }
+                });
+                if(time > 5){
+                    delay = 1200;
                     mediaSound.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                         @Override
                         public void onCompletion(MediaPlayer mediaPlayer) {
@@ -115,36 +156,15 @@ public class MainActivity extends Activity{
                         }
                     });
                     ParticleSystem ps = new ParticleSystem(MainActivity.this, 100, R.drawable.autumn_red, 5000);
-                    int ab = r.nextInt(7) + 1;
-                    if(ab == 1){
-                        ps = new ParticleSystem(MainActivity.this, 100, R.drawable.s_red, 5000);
-                    }
-                    else if(ab == 2){
-                        ps = new ParticleSystem(MainActivity.this, 100, R.drawable.s_blue, 5000);
-                    }
-                    else if(ab == 3){
-                        ps = new ParticleSystem(MainActivity.this, 100, R.drawable.s_green, 5000);
-                    }
-                    else if(ab == 4){
-                        ps = new ParticleSystem(MainActivity.this, 100, R.drawable.s_white, 5000);
-                    }
-                    else if(ab == 5){
-                        ps = new ParticleSystem(MainActivity.this, 100, R.drawable.s_yellow, 5000);
-                    }
-                    else if(ab == 6){
-                        ps = new ParticleSystem(MainActivity.this, 100, R.drawable.s_purple, 5000);
-                    }
-                    else if(ab == 7){
-                        ps = new ParticleSystem(MainActivity.this, 100, R.drawable.s_pink, 5000);
-                    }
+                    int ab = r.nextInt(7);
+                    ps = new ParticleSystem(MainActivity.this, 100, view[pos][ab], 5000);
 
                     ps.setScaleRange(0.7f, 1.3f);
                     ps.setSpeedRange(0.1f, 0.25f);
                     ps.setAcceleration(0.0001f, 90);
                     ps.setRotationSpeedRange(90, 180);
                     ps.setFadeOut(200, new AccelerateInterpolator());
-                    ps.oneShot(tx, 100);
-
+                    ps.oneShot(tx, 70);
 
                 }
                 else if(time > 15){
